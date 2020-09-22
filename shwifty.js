d16b0d479a85ea20a71a97b83e6199f115448025
unknown <theLittleCitizen6@gmail.com>
2020-09-22 13:08:02 +0300

    before iso

--- shwifty.js
+++ shwifty.js
@@ -18,7 +18,6 @@ function randerNumbers(colNum){
             arr.push(numbers[index]);
         }
         numbers.splice(index, 1);
-        console.log(numbers);
     }
     return arr;
 }
@@ -33,14 +32,23 @@ function getSolvableForOdds(colNum){
 function oppositeCount(arr){
     var counter = 0;
     for (var i = 0;i< arr.length;i++ ){
-        for (var j = i+1;j< arr.length;j++ ){
-            if(arr[j] > arr[i]){
+        for (var j = i;j< arr.length;j++ ){
+            if(arr[j] < arr[i] && arr[i] != "" && arr[j] != ""){
                 counter++;
+                // console.log(counter + "          " + i + " => " +arr[i] + " , " + j +" => " + arr[j])
             }
         }
     }
     return counter;
 }
+
+function isValid(arr, index){
+    if (arr[index] == index +1 || (arr[index] == "" && index == arr.length - 1)){
+        return true;
+    }
+    return false;
+}
+
 
 function randerRow(colNum, numbers) {
     var row = document.createElement("div")
@@ -63,4 +71,62 @@ function randerBoard(colNum){
     for (var i =0; i<colNum; i++){
         board.appendChild(randerRow(colNum, numbers));
     }
+}
+function toArray(array2d){
+    var arr = [];
+    for(var i = 0; i < array2d.length; i++)
+    {
+        arr = arr.concat(array2d[i]);
+    }
+    return arr;
+}
+function getBoxes(){
+    var boxes = [];
+    rows = document.getElementsByClassName("row");
+    for (var i =0 ; i<rows.length; i++){
+        var arr = [];
+        for (var j=0 ;j<rows[i].childNodes.length; j++){
+            arr.push(rows[i].childNodes[j]);
+        }
+        boxes.push(arr);
+    }
+    return boxes;
+}
+function getBoardState(){
+    var boxes = getBoxes();
+    var boardState = [];
+    for (var i =0 ; i<boxes.length; i++){
+        var arr = [];
+        for (var j=0 ;j<boxes[i].length; j++){
+            arr.push(boxes[i][j].firstChild.textContent);
+        }
+        boardState.push(arr);
+    }
+    return boardState;
+}
+
+function updateBoardState(arr2d){
+    rows = document.getElementsByClassName("row");
+    for (var i =0 ; i<rows.length; i++){
+        for (var j=0 ;j<rows[i].childNodes.length; j++){
+            rows[i].childNodes[j].firstChild.textContent = arr2d[i][j];
+        }
+    }
+}
+
+function updateBoardStateBoxes(boxes){
+    rows = document.getElementsByClassName("row");
+    for (var i =0 ; i<rows.length; i++){
+        for (var j=0 ;j<rows[i].childNodes.length; j++){
+            rows[i].childNodes[j].replaceWith(boxes[i*rows[i].childNodes.length + j]);
+        }
+    }
+}
+
+function SwitchWithEmpty(box, boxes){
+    var oldEmpty = boxes.filter(box => box.firstChild.textContent == "")[0];
+    var temp = box;
+    boxes[boxes.indexOf(box)] = oldEmpty;
+    boxes[boxes.indexOf(oldEmpty)]= temp;
+    updateBoardStateBoxes(boxes);
 }
